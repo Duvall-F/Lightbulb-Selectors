5eab44968e5e48bc4b3f58ef1cb97dfcdf3d267d
Duvall-F <flandrin2020@gmail.com>
2024-10-17 15:11:59 +0100

    added javascript code

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Write your code here
+let count = 0
+const lightBulb1 = document.querySelector("#lightbulb1");
+const lightBulb2 = document.querySelector("#lightbulb2");
+const lightBulb3 = document.querySelector("#lightbulb3");
+const subtitle = document.querySelector(".subtitle")
+
+lightBulb1.addEventListener("click", function(){
+    count++;
+    subtitle.innerHTML = "You've clicked the lights " + count + " times";
+    lightBulb1.classList.toggle("active");
+});
+
+lightBulb2.addEventListener("click", function(){
+    count++;
+    subtitle.innerHTML = "You've clicked the lights " + count + " times";
+    lightBulb2.classList.toggle("active");
+});
+
+lightBulb3.addEventListener("click", function(){
+    count++;
+    subtitle.innerHTML = "You've clicked the lights " + count + " times";
+    lightBulb3.classList.toggle("active");
+});
